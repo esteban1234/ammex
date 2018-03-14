ad1d0f13294afa5b1dcab86074cdf99c90f32176
esteban1234 <tva_libra@hotmail.com>
2018-03-13 18:22:09 -0600

    PAGINA LISTA FALTA GALERIA

--- vistas/nosotros.php
+++ vistas/nosotros.php
@@ -56,34 +56,101 @@
           </div>
         </div> <br><br>
         <div class="row">
-          <div class="col-md-2">
-            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/user_placeholder.png"
-            class="img-circle img-responsive">
+          <div class="col-md-1">
+            <i class="fas fa-user-plus fa-5x" style="color: #ED6A16;"></i>
           </div>
-          <div class="col-md-2">
+          <div class="col-md-3">
             <h3 class="text-left">Atención personalizada</h3>
-            <p class="text-left">Lorem ipsum dolor sit amet, adipiscing elit Aenean commodo ligula eget.</p>
+            <p class="text-left">Siempre asesoramiento personalizado por profesionales expertos en Viajes.</p>
           </div>
-          <div class="col-md-2">
-            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/user_placeholder.png"
-            class="img-circle img-responsive">
+          <div class="col-md-1">
+            <i class="fas fa-thumbs-up fa-5x" style="color: #ED6A16;"></i>
           </div>
-          <div class="col-md-2">
+          <div class="col-md-3">
             <h3 class="text-left">La mejor atención post-venta</h3>
-            <p class="text-left">Lorem ipsum dolor sit amet, adipiscing elit Aenean commodo ligula eget.</p>
+            <p class="text-left">Ofreciéndote la mejor atención, las mejores ofertas de viajes y los mejores precios.</p>
           </div>
-          <div class="col-md-2">
-            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/user_placeholder.png"
-            class="img-circle img-responsive">
+          <div class="col-md-1">
+            <i class="fas fa-credit-card fa-5x" style="color: #ED6A16;"></i>
           </div>
-          <div class="col-md-2">
+          <div class="col-md-3">
             <h3 class="text-left">Los mejores precios</h3>
-            <p class="text-left">Lorem ipsum dolor sit amet, adipiscing elit Aenean commodo ligula eget.</p>
+            <p class="text-left">Trabajamos con Todas las Tarjetas y Bancos para tus vacaciones soñadas.</p>
           </div>
         </div>
         
       </div>
     </div>
+
+    <div class="section" style="padding: 5% 0%; background-color: #ED6A16;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <figure class="logo2">
+			<img src="../img/ammex.jpg" alt="AMMEX" class="img-responsive">
+			</figure>
+            <h2 style="color: #fff;">NOSOTROS</h2>
+            <p style="color: #fff;">AMMEX es una agencia de viajes. Una empresa que cuenta con experiencia en el ramo de turismo, especializados en viajes grupales y personalizados alrededor del mundo. Nos distingue una excelente atención al cliente, su satisfacción siempre es nuestra prioridad; por lo mismo ofrecemos servicios integrales en el exterior, sin intermediarios y siempre acompañados por un miembro de la empresa desde el inicio del viaje.</p>
+          </div>
+          <div class="col-md-4">
+            <div class="iconos" style=" text-align: center;">
+            	<i class="fas fa-handshake fa-5x" style="color: #fff;"></i>
+            </div>
+            <h2 style="color: #fff;">MISION</h2>
+            <p style="color: #fff;">Satisfacer las necesidades de nuestros clientes a través de la prestación de un servicio profesional, eficiente y personalizado ofreciendo las mejores opciones, tarifas y horarios en: excursiones, paquetes, boletos de avión, cruceros y más.</p>
+          </div>
+          <div class="col-md-4">
+            <div class="iconos" style=" text-align: center;">
+            	<i class="fas fa-chart-line fa-5x" style="color: #fff;"></i>
+            </div>
+            <h2 style="color: #fff;">VISION</h2>
+            <p style="color: #fff;">Ser una empresa líder, brindando al cliente una satisfacción total, asimismo seguir proporcionando un servicio de calidad y seguridad que beneficie a la empresa, pero sobre todo a los usuarios de nuestro servicio</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="section" style="padding: 5% 0%;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1 class="text-center">¡Ellos ya confiaron en nosotros!</h1>
+            <!-- <p class="text-center">Expertos en viajes</p> -->
+          </div>
+        </div> <br><br>
+        <div class="row">
+          <div class="col-md-1">
+            <i class=" fas fa-user-circle fa-5x" style="color: #ED6A16;"></i>
+          </div>
+          <div class="col-md-3">
+            <h3 class="text-left">Raul</h3>
+            <p class="text-left">Todo resultó tal como estaba planificado y detallado en el itinerario, totalmente recomendables y confiables.</p>
+          </div>
+          <div class="col-md-1">
+            <i class="fas fa-user-circle fa-5x" style="color: #ED6A16;"></i>
+          </div>
+          <div class="col-md-3">
+            <h3 class="text-left">Carlos</h3>
+            <p class="text-left">Excelente agencia, la recomiendo 100%. Todo resultó perfecto.</p>
+          </div>
+          <div class="col-md-1">
+            <i class="fas fa-user-circle fa-5x" style="color: #ED6A16;"></i>
+          </div>
+          <div class="col-md-3">
+            <h3 class="text-left">Veronica</h3>
+            <p class="text-left">Es la primera vez que viajo con esta Agencia y todo me resultó como uno espera: todo como un reloj de arena.</p>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+
+    <div class="container" style="margin-bottom: 2%;">
+	<div class="row text-center">
+            <a href="contacto.php" class="btn btn-warning btn-outline btn-lg">CONTACTANOS</a>
+	</div>
+</div>
 
 <a class="go-top" href="#">Subir</a>
 <?php include 'pie.php' ?>
