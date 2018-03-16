005350cb784a537b995e98a89dafb6de84d218d0
esteban1234 <tva_libra@hotmail.com>
2018-03-16 09:31:33 -0600

    metas

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="wrJ9QWGEwOYxrsazOAQEqm10ggZ0bCdqngadrSe059k" />
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<title>AMMEX TURISMO MULTISERVICIO</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
